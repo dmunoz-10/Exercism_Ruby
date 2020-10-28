2375d59fccdbb52948f4108d23283e95f0f0642b
Daniel Muñoz <djmv225@gmail.com>
2020-10-27 22:03:09 -0500

    Allergies exercise done

--- allergies/allergies.rb
+++ allergies/allergies.rb
@@ -1,28 +1,27 @@
 # Allergies Class
 class Allergies
+  ALLERGIES = { cats: 128, pollen: 64,
+                chocolate: 32, tomatoes: 16,
+                strawberries: 8, shellfish: 4,
+                peanuts: 2, eggs: 1 }.freeze
+
   attr_reader :list
-  ITEMS = { cats: 128, pollen: 64,
-            chocolate: 32, tomatoes: 16,
-            strawberries: 8, shellfish: 4,
-            peanuts: 2, eggs: 1 }.freeze
 
   def initialize(score)
     @list = []
-    score_temp = score
-    ITEMS.each do |item, value|
+    score_temp = score > 256 ? score - 256 : score
+
+    ALLERGIES.each do |item, value|
       if score_temp >= value
         @list << item.to_s
         score_temp -= value
       end
     end
+
     @list.reverse!
   end
 
-  def allergic_to?(item)
-    @list.include?(item)
+  def allergic_to?(allergen)
+    @list.include?(allergen)
   end
-end
-
-module BookKeeping
-  VERSION = 1
 end
