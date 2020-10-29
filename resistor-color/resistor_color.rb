17325bf37ebf4a3103a7cae21c6f4b0934118ba6
Daniel Muñoz <djmv225@gmail.com>
2020-10-29 00:07:18 -0500

    Resistor color exercise done

---
+++ resistor-color/resistor_color.rb
@@ -0,0 +1,8 @@
+# ResistorColor module
+module ResistorColor
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+
+  def self.color_code(color)
+    COLORS.index(color)
+  end
+end
