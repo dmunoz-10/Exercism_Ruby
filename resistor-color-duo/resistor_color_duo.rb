46000de86d3769e00541dbde7fa57a1f83efe87b
Daniel Muñoz <djmv225@gmail.com>
2020-10-29 20:38:44 -0500

    Resistor color duo exercise updated

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -3,8 +3,6 @@ module ResistorColorDuo
   COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.value(colors)
-    colors[0..1].each_with_object([]) do |color, value|
-      value << COLORS.index(color)
-    end.join.to_i
+    "#{COLORS.index(colors[0])}#{COLORS.index(colors[1])}".to_i
   end
 end
