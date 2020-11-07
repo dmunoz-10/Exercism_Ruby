d14e5029dc1f51e2cab5f1b47c880f688d22121e
Daniel Muñoz <djmv225@gmail.com>
2020-11-07 11:12:50 -0500

    Adding high scores exercise

---
+++ high-scores/high_scores.rb
@@ -0,0 +1,24 @@
+# HighScores Class
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.sort.reverse.first(3)
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
