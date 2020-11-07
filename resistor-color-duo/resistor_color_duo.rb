dd99bfbeb9444841604600ff35be19575ddc280e
Daniel Muñoz <djmv225@gmail.com>
2020-11-06 23:00:23 -0500

    Improving the resistor color duo exercise Interpolation was changed by arithmetic

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -3,6 +3,6 @@ module ResistorColorDuo
   COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.value(colors)
-    "#{COLORS.index(colors[0])}#{COLORS.index(colors[1])}".to_i
+    (COLORS.index(colors[0]) * 10) + COLORS.index(colors[1])
   end
 end
