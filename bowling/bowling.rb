57142c623c618e90b86a3db3d323075b2faa6503
Daniel Muñoz <djmv225@gmail.com>
2020-11-20 13:18:43 -0500

    Finishing bowling exercise

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,60 +1,73 @@
-# Game Class - UNFINISHED
+# Game Class
 class Game
   BowlingError = Class.new(StandardError)
 
   def initialize
-    @frames = []
+    @frames = Array.new(11) { Frame.new }
     @i = 0
   end
 
   def roll(pins)
-    raise BowlingError unless (0..10).cover?(pins)
-    raise BowlingError if full?
-    @frames << pins
+    raise BowlingError if closed_game?
+
+    @frames[@i] << pins
+    @frames[0...@i].each { |frame| frame << pins if frame.add_bonus? }
+    @i += 1 if @frames[@i].closed_frame? && @i != 10
   end
 
   def score
-    full?
-    validate
-    @i = 0
-    (1..10).each_with_object([]) { |_x, res| res << proccess(@i) }.sum
+    raise BowlingError unless closed_game?
+
+    @frames[0...10].sum(&:score)
   end
 
-  def proccess(ind)
-    if @frames[ind] == 10
-      @i += 1
-      10 + @frames[ind + 1] + @frames[ind + 2]
-    elsif @frames[ind] + @frames[ind + 1] == 10
-      @i += 2
-      10 + @frames[ind + 2]
-    else
-      @i += 2
-      @frames[ind] + @frames[ind + 1]
+  private
+
+  def closed_game?
+    @frames[0...10].all? { |frame| frame.closed_frame? && !frame.add_bonus? }
+  end
+
+  # Frame Class
+  class Frame
+    def initialize
+      @rolls = []
+    end
+
+    def <<(pins)
+      raise BowlingError unless pins.between?(0, 10)
+
+      @rolls << pins
+
+      raise BowlingError if invalid_frame?
+    end
+    alias push <<
+
+    def score
+      @rolls.sum
+    end
+
+    def strike?
+      @rolls[0] == 10
+    end
+
+    def spare?
+      @rolls[0..1].sum == 10
+    end
+
+    def common_frame?
+      @rolls.size == 2 && @rolls.sum < 10
+    end
+
+    def invalid_frame?
+      !strike? && !spare? && @rolls.size <= 2 && !@rolls.sum.between?(0, 10)
+    end
+
+    def closed_frame?
+      common_frame? || strike? || spare?
+    end
+
+    def add_bonus?
+      (strike? || spare?) && @rolls.size < 3
     end
   end
-
-  def full?
-    return false if @frames.length < 11
-    index = 0
-    
-  end
-
-  def validate
-    raise BowlingError if @frames.empty?
-    10.times do
-      if @frames[@i] == 10
-        @i += 1
-        next
-      end
-      if @frames[@i] + @frames[@i + 1] < 11
-        @i += 2
-        next
-      end
-      raise BowlingError
-    end
-  end
-end
-
-module BookKeeping
-  VERSION = 3
 end
