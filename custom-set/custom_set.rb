f1719b05d6fc66f4471b241dd2b1df834cc02ded
Daniel Muñoz <djmv225@gmail.com>
2020-11-20 22:31:48 -0500

    Adding custom set exercise

---
+++ custom-set/custom_set.rb
@@ -0,0 +1,54 @@
+# CustomSet Class
+class CustomSet
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def add(item)
+    data << item unless data.include?(item)
+
+    self
+  end
+  alias << add
+
+  def member?(item)
+    data.include?(item)
+  end
+
+  def empty?
+    data.empty?
+  end
+
+  def subset?(other)
+    data.all? { |item| other.member?(item) }
+  end
+
+  def disjoint?(other)
+    (self & other).empty?
+  end
+
+  def &(other)
+    data & other.data
+  end
+
+  def ==(other)
+    data.sort == other.data.sort
+  end
+  alias eql ==
+
+  def intersection(other)
+    CustomSet.new(self & other)
+  end
+
+  def difference(other)
+    CustomSet.new(data.difference(other.data))
+  end
+  alias - difference
+
+  def union(other)
+    CustomSet.new((data + other.data).uniq)
+  end
+  alias + difference
+end
