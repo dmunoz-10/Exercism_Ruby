5cea43df7af73c3d820758ff8db4abf6b2c24d74
Daniel Muñoz <djmv225@gmail.com>
2021-12-05 13:10:48 -0500

    Adds the boutique inventory exercise

---
+++ boutique-inventory/boutique_inventory.rb
@@ -0,0 +1,33 @@
+class BoutiqueInventory
+  def initialize(items)
+    @items = items
+  end
+
+  def item_names
+    items.map { |item| item[:name] }.sort
+  end
+
+  def cheap
+    items.select { |item| item[:price] < 30 }
+  end
+
+  def out_of_stock
+    items.select { |item| item[:quantity_by_size].empty? }
+  end
+
+  def stock_for_item(name)
+    item = items.find { |item| item[:name] == name }
+    return unless item
+
+    item[:quantity_by_size]
+  end
+
+  def total_stock
+    items.sum do |item|
+      item[:quantity_by_size].sum { |_k, v| v }
+    end
+  end
+
+  private
+  attr_reader :items
+end
