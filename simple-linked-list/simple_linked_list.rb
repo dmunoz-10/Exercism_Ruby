a60b60518b093a7a69e8bc93a62bf4425982554f
Daniel Muñoz <djmv225@gmail.com>
2022-01-15 12:58:18 -0500

    Fixes the simple linked list exercise

--- simple-linked-list/simple_linked_list.rb
+++ simple-linked-list/simple_linked_list.rb
@@ -1,33 +1,72 @@
 class SimpleLinkedList
   def initialize(list = nil)
-    @list = []
+    @head = nil
+    @tail = nil
     list.each { |e| push(Element.new(e)) } if list
   end
 
   def push(element)
-    @list.unshift(element)
+    @tail.next = element if @tail
+    @tail = element
+    @head = element unless @head
 
     self
   end
   alias :<< :push
 
   def pop
-    @list.shift
+    if @head == @tail
+      node = @head
+      @head = nil
+      @tail = nil
+    else
+      node = @tail
+      @tail = element_before(node)
+      @tail.next = nil
+    end
+
+    node
   end
 
   def to_a
-    @list.map(&:datum)
+    get_array.reverse.map(&:datum)
   end
 
   def reverse!
-    @list.reverse!
+    array = get_array.each { |e| e.next = nil }
+    @head = nil
+    @tail = nil
+    array.reverse.each { |e| push(e) }
 
     self
+  end
+
+  private
+
+  def element_before(node)
+    temp = @head
+    until temp.next == node
+      temp = temp.next
+    end
+
+    temp
+  end
+
+  def get_array
+    temp = @head
+    array = []
+    until temp.nil?
+      array << temp
+      temp = temp.next
+    end
+
+    array
   end
 end
 
 class Element
-  attr_accessor :datum, :next
+  attr_accessor :next
+  attr_reader :datum
 
   def initialize(datum)
     @datum = datum
